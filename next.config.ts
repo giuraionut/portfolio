5e8e69e8355487cb4aa591e7ca5e18ce86e74f4d
giuraionut98 <giuraionut98@gmail.com>
2025-05-04 13:36:35 +0300

    v0.2.0

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
+import { version } from "./package.json";
 
 const nextConfig: NextConfig = {
+  env: {
+    version
+  },
   /* config options here */
 };
 
